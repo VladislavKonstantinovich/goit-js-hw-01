63ef474e7d6fe0780dd360663cbccad14dabbdf9
VladislavKonstantinovich <83119196+VladislavKonstantinovich@users.noreply.github.com>
2021-09-25 10:57:38 +0300

    Add files via upload

--- js/task-6.js
+++ js/task-6.js
@@ -5,8 +5,7 @@ while(input !== null){
     input = prompt('Введіть число');
     if(input === null){
         break;
-    }
-    if(isNaN(Number.parseFloat(input))){
+    } else if(isNaN(parseFloat(input))){
         alert('Помилка, спробуйте знова');
         continue;
     }
@@ -14,68 +13,3 @@ while(input !== null){
 }
 
 alert('Вийшло число ' + total);
-
-
-
-
-
-// let input = prompt('Введіть число');
-// let number = Number(input);
-// let isNumber = isNaN(number);
-// let total = 0;
-// total = total + number;
-// console.log(total);
-
-// while (isNumber === false) {
-//     let input = prompt('Введіть число');
-//     let number = Number(input);
-//     if (isNumber === false) {
-//         total = total + number;
-//         console.log(total);
-//     }
-    
-// }
-
-
-
-// do {
-//     isnan = isNaN(input);
-//     console.log(isnan);
-//     if (isnan === true && input !== null) {
-//         input = parseFloat(prompt('Введіть число'));
-//         number = parseFloat(input);
-//         total = total + number;
-//         console.log(total);
-//     }
-// } while (isnan === true && input !== null);
-
-
-// do {
-//     if (input === number && input !== NaN) {
-//         input = prompt('Введіть число');
-//         number = parseFloat(input);
-//         total = total + number;
-//         console.log(total);
-//     }
-// } while (input === number && input !== NaN);
-
-
-
-// do {
-//     if (number !== null) {
-//         number = parseInt(prompt('Введіть число'));
-//         total = total + number;
-//         console.log(total);
-//     } else
-// } while (number !== null);
-
-
-
-
-
-
-
-// while (number !== null) {
-//     number = parseFloat(number);
-//     total = total + number;
-// }
